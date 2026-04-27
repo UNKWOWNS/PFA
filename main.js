f6f0e7e6665bf3a1be058af2217f3a4e1545163f
UNKWOWNS <andreidobrin1923@gmail.com>
2026-04-27 03:11:24 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Smooth scroll (optional future enhancements)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
-  });
-});
